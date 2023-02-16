20b53e6b9166f0f7c9afde50da166256705a1bac
GTT\crodriguez <crodriguez@gtt.es>
2023-02-16 19:40:25 +0100

    Avance punto 1

--- TComplejo.h
+++ TComplejo.h
@@ -20,7 +20,35 @@ public:
 
     TComplejo(TComplejo&);
 
-    TComplejo& operator=(const TComplejo&);
+    TComplejo& operator=(TComplejo&);
+
+    TComplejo operator+ (TComplejo&);
+    TComplejo operator- (TComplejo&);
+    TComplejo operator* (TComplejo&);
+    TComplejo operator+ (double);
+    TComplejo operator- (double);
+    TComplejo operator* (double);
+
+    bool operator== (TComplejo&);
+    bool operator!= (TComplejo&);
+
+    double Re();
+    double Im();
+
+    void Re(double);
+    void Im(double);
+
+    double Arg(void);
+    double Mod(void);
+
+    friend ostream & operator<<(ostream &, TComplejo &);
+
+    friend TComplejo operator+ (double, TComplejo&);
+    friend TComplejo operator- (double, TComplejo&);
+    friend TComplejo operator* (double, TComplejo&);
+
+
+
 };
 
 #endif;
