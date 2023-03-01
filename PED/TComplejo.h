2c7358c93446cff744dfb3087daabff1d63071b6
crg85-ua <78562703+crg85-ua@users.noreply.github.com>
2023-03-01 13:38:25 +0100

    Practica 1 de PED

---
+++ PED/TComplejo.h
@@ -0,0 +1,54 @@
+#ifndef _TCOMPLEJO
+#define _TCOMPLEJO
+#include<iostream>
+
+using namespace std;
+
+class TComplejo
+{
+private:
+    double re;
+    double im;
+public:
+    TComplejo();
+
+    ~TComplejo();
+
+    TComplejo(double);
+
+    TComplejo(double , double);
+
+    TComplejo(TComplejo&);
+
+    TComplejo& operator=(TComplejo&);
+
+    TComplejo operator+ (TComplejo&);
+    TComplejo operator- (TComplejo&);
+    TComplejo operator* (TComplejo&);
+    TComplejo operator+ (double);
+    TComplejo operator- (double);
+    TComplejo operator* (double);
+
+    bool operator== (TComplejo&);
+    bool operator!= (TComplejo&);
+
+    double Re();
+    double Im();
+
+    void Re(double);
+    void Im(double);
+
+    double Arg(void);
+    double Mod(void);
+
+    friend ostream & operator<<(ostream &, TComplejo &);
+
+    friend TComplejo operator+ (double, TComplejo&);
+    friend TComplejo operator- (double, TComplejo&);
+    friend TComplejo operator* (double, TComplejo&);
+
+
+
+};
+
+#endif;
