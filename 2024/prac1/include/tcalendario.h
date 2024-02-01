fb07c538e2b0277f03764da5e107ce7a6a4fc83d
GTT\crodriguez <crodriguez@gtt.es>
2024-02-01 12:02:43 +0100

    incio cuad1

---
+++ 2024/prac1/include/tcalendario.h
@@ -0,0 +1,73 @@
+#ifndef _TCALENDARIO
+#define _TCALENDARIO
+#include <ostream>
+
+using namespace std;
+
+class TCalendario
+{
+private:
+    int dia, mes, anyo;
+    char* mensaje;
+public:
+    //Constructor por defecto
+    TCalendario();
+    //Constructor con parámetros
+    TCalendario(int, int, int, char *);
+    //Constructor copia
+    TCalendario(const TCalendario &);
+    //Destructor
+    ~TCalendario();
+    //Sobrecarga del operador =
+    TCalendario operator=(const TCalendario&);
+
+    //METODOS
+    //Sobrecarga del operador +
+    TCalendario operator+(const TCalendario&);
+
+    //Sobrecarga del operador -
+    TCalendario operator-(const TCalendario&);
+
+    //Sobrecarga del operador ++ (postincremento)
+    TCalendario operator++(int);
+
+    //Sobrecarga del operador ++ (preincremento)
+    TCalendario& operator++();
+
+    //Sobrecarga del operador -- (postdecremento)
+    TCalendario operator--(int);
+
+    //Sobrecarga del operador -- (predecremento)
+    TCalendario& operator--();
+
+    //Modifica el mensaje
+    bool ModMEnsaje(char *);
+
+    //Sobrecarga del operador ==
+    bool operator==(const TCalendario&);
+
+    //Sobrecarga del operador !=
+    bool operator!=(const TCalendario&);
+
+    //Sobrecarga del operador <
+    bool operator<(const TCalendario&);
+
+    //Sobrecarga del operador >
+    bool operator>(const TCalendario&);
+
+    //Devuelve el mensaje
+    char* Mensaje();
+
+    friend ostream & operator<<(ostream &, const TCalendario &);
+
+};
+
+#endif
+
+// tcalendario::tcalendario(/* args */)
+// {
+// }
+
+// tcalendario::~tcalendario()
+// {
+// }
